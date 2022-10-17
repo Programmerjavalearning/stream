2046126be870a96a28a32ba7bbdb08b0252abcb5
Nicolas <112377899+Programmerjavalearning@users.noreply.github.com>
2022-10-17 14:20:34 +0200

    Dynamic streams

--- FunctionalProgramming.java
+++ FunctionalProgramming.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class FunctionalProgramming {
@@ -15,6 +16,35 @@ public class FunctionalProgramming {
         System.out.println(Arrays.stream(numberArray).sum());
 
         System.out.println(Arrays.stream(numberArray).average());
+
+        // Stream de 1 à 9
+        System.out.println(IntStream.range(1,10).sum());
+        // Stream de 1 à 10
+        System.out.println(IntStream.rangeClosed(1,10));
+
+        System.out.println(IntStream.rangeClosed(1,10).sum());
+
+
+        //Créé une stream de 10 éléments qui commence à 1 et incrémente de +2 chaque élément
+        System.out.println(IntStream.iterate(1, e -> e + 2).limit(10).peek(System.out::println).sum());
+
+
+        System.out.println(IntStream.iterate(2, e -> e + 2).limit(10).peek(System.out::println).sum());
+
+        // Le cube de chaque éléments
+        System.out.println(IntStream.iterate(2, e -> e * 2).limit(10).peek(System.out::println).sum());
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
